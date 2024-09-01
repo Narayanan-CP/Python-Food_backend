5edd840ec4a156d46395be474cd44040c2eea1c7
narayanan.cp.official@gmail.com <narayanan.cp.official@gmail.com>
2024-09-01 21:07:11 +0530

    init com

---
+++ PythonProject/Models/UserManager.py
@@ -0,0 +1,18 @@
+from Models.User import User
+class UserManager:
+    __Users=[]
+    @classmethod
+    def AddUser(cls,userObj):
+        if isinstance(userObj,User):
+            cls.__Users.append(userObj)
+            print("You have been Succeddfully registered")
+        else:
+            print("Invalid User")
+    @classmethod
+    def FindUser(cls,mailid,pwd):
+        for user in cls.__Users:
+            if user.MailId==mailid and user.Password==pwd:
+                return user        
+        
+        
+            
